37f1174c0997144bf2cc23f296ee55df417327e7
Igor Khotin <khotin@gmx.com>
2020-02-01 17:41:26 +0200

    [+] nanobot sprite view

--- dna/Nanobot.js
+++ dna/Nanobot.js
@@ -1,27 +1,34 @@
+const BORDER = rx(.05)
 
 class Nanobot {
     
     constructor() {
         this.Z = 11
         this.name = 'nanobot'
+        this.r = ry(.04),
         this.x = rx(.5),
-        this.y = ry(.9),
-        this.r = ry(.025),
+        this.y = ry(1) - this.r,
         this.color = hsl(.55, .4, .4)
     }
 
     evo(dt) {
-        this.x = limit(mouse.x, 0, width())
+        this.x = limit(mouse.x,
+            this.r + BORDER,
+            rx(1) - this.r - BORDER)
     }
 
     draw() {
         const { x, y, r } = this
 
+        blocky()
+        image(res.sprites.bot, x-r, y-r, 2*r, 2*r)
+        /*
         fill(this.color)
         rect(x-r, y-r, 2*r, 2*r)
 
         lineWidth(4)
         stroke(this.color)
         line(x, y-r, x, y-r*1.5)
+        */
     }
 }
--- trap/start.js
+++ trap/start.js
@@ -2,6 +2,8 @@
 // traps the start of the game
 function start() {
 
+    lib.util.hideCursor()
+
     // create dna and nanobot
     lab.spawn('Nanobot')
 }
