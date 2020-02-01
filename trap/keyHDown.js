305c9351c28c1d4b7ea12e62bf20d29e2d8c2c00
Anatolii Yakushko <shaddyx@gmail.com>
2020-02-01 23:41:22 +0200

    [+] added tree walkthrough

--- trap/keyHDown.js
+++ trap/keyHDown.js
@@ -1,6 +1,7 @@
 module.exports = function(e) {
     if (e.repeat) return
-    let toDamage = lib.math.rnde($.lab._ls.filter(o => !o.leftDamaged || !o.rightDamaged ));
+    let nodes = lib.util.findNode(o => o instanceof dna.Nucleotide);
+    let toDamage = lib.math.rnde(nodes.filter(o => !o.leftDamaged || !o.rightDamaged ));
     if (!toDamage.leftDamaged){
         sys.spawn(dna.Radiation, {
             target: toDamage,
