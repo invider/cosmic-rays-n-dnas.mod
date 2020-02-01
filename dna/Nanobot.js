880eb2d97fc723e9e4a3ff3a664848b34cccc435
Igor Khotin <khotin@gmx.com>
2020-02-01 19:17:53 +0200

    [+] bot movement within cosmos

--- dna/Nanobot.js
+++ dna/Nanobot.js
@@ -3,25 +3,36 @@ class Nanobot {
     constructor(st) {
         this.Z = 11
         this.name = 'nanobot'
-        this.r = 32,
+        this.r = 16,
         this.x = 0
         this.y = 0,
-
+            
+        this.w = res.sprites.bot.width
+        this.h = res.sprites.bot.height
         augment(this, st)
     }
 
+    init() {
+        this.x = this.__.rx(.5)
+        this.y = this.__.ry(1) - this.h/2
+    }
+
     evo(dt) {
-        /*
-        this.x = limit(mouse.x,
-            this.r + env.tune.border,
-            rx(1) - this.r - env.tune.border)
-        */
+        const edge = env.tune.border + this.w/2
+        this.x = limit($.cosmos.worldX(mouse.x),
+            edge, $.cosmos.rx(1) - edge)
     }
     
     draw() {
-        const { x, y, r } = this
+        const { x, y, w, h } = this
 
         blocky()
-        image(res.sprites.bot, x-r, y-r, 2*r, 2*r)
+        image(res.sprites.bot, x-w/2, y-h/2, w, h)
+
+        /*
+        stroke('#ffff00')
+        lineWidth(1)
+        rect(x-w/2, y-h/2, w, h)
+        */
     }
 }
---
+++ lab/stat.js
@@ -0,0 +1,6 @@
+function evo(dt) {
+    env.statusInfo = {
+        x: round($.cosmos.worldX(mouse.x)),
+        y: round($.cosmos.worldY(mouse.y)),
+    }
+}
