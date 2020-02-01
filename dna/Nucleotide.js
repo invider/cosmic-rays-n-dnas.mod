84d450924c90cb7bcc6757c1f6daaedd96e1b458
Anatolii Yakushko <shaddyx@gmail.com>
2020-02-01 17:54:13 +0200

    [-] fixed sizing

--- dna/Nucleotide.js
+++ dna/Nucleotide.js
@@ -22,15 +22,15 @@ class Nucleotide {
         let offset = this.x / 100 + this.rotation;
         let angle = offset;
         let multiplier = Math.cos(angle);
-        let sizeMultiplier = (Math.cos(angle - Math.PI / 2) + 2) / 2;
-        let size2Multiplier = (Math.cos(angle + Math.PI / 2) + 2) / 2;
+        let sizeMultiplier = Math.cos(angle - Math.PI / 2) + 1;
+        let size2Multiplier = Math.cos(angle + Math.PI / 2 + 1);
         let distance = this.size * multiplier;
         stroke(this.map[this.left][1]);
         line(this.x, this.y - distance, this.x, this.y);
         stroke(this.map[this.right][1]);
         line(this.x, this.y + distance, this.x, this.y);
-        let size1 = 32 * sizeMultiplier;
-        let size2 = 32 * size2Multiplier;
+        let size1 = 28 + 8 * sizeMultiplier;
+        let size2 = 28 + 8 * size2Multiplier;
 
         blocky()
         image(this.map[this.left][0], this.x - size1 / 2, this.y - distance - size1 / 2, size1, size1);
