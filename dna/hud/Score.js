662a3dc8e79e4ad91f97959fe4cea27a6ca059d6
Anatolii Yakushko <shaddyx@gmail.com>
2020-02-02 12:31:55 +0200

    [-] fixed font

--- dna/hud/Score.js
+++ dna/hud/Score.js
@@ -17,10 +17,8 @@ class Score {
         let currentDamaged = env.state.damaged - env.state.repaired;
         let percent = Math.floor(currentDamaged / atoms * 100);
 
-        fill("blue");
-        text(`L: ${100 - percent}%, M: ${percent}%, S: ${env.state.repaired}`, this.x, this.y + 15);
-        // text(`R: ${env.state.repaired}`, this.x, this.y + 35);
-        // text(`T: ${env.state.damaged}`, this.x, this.y + 55);
-        // text(`P: ${percent} %`, this.x, this.y + 75);
+        fill("white");
+        font(`${64 / $.cosmos.scale}px coolville`)
+        text(`L: ${100 - percent}% M: ${percent}% S: ${env.state.repaired}`, this.x, this.y + 15);
     }
 }
