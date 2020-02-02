c4ae574addd14d8243d25eb17746dc8be2545c74
Anatolii Yakushko <shaddyx@gmail.com>
2020-02-02 13:35:10 +0200

    [+] added hud

--- dna/hud/Score.js
+++ dna/hud/Score.js
@@ -16,9 +16,9 @@ class Score {
         let atoms = nodes.length * 2;
         let currentDamaged = env.state.damaged - env.state.repaired;
         let percent = Math.floor(currentDamaged / atoms * 100);
-
+        image(res.ui.hud, 0, this.y, 320, 16);
         fill("white");
         font(`${32 / $.cosmos.scale}px coolville`)
-        text(`Life: ${100 - percent}% Mutation: ${percent}% Score: ${env.state.repaired}`, this.x, this.y + 15);
+        text(`Life: ${100 - percent}% Mutation: ${percent}% Score: ${env.state.repaired}`, this.x + 10, this.y + 9);
     }
 }
