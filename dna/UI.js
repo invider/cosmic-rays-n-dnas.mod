46c95b08d8f382220273a135991a73c1852c8725
Igor Khotin <khotin@gmx.com>
2020-02-02 12:38:08 +0200

    [+] start screen container

--- dna/UI.js
+++ dna/UI.js
@@ -1,13 +1,41 @@
-class UI {
+class UI extends dna.hud.Container {
+
   constructor(st) {
-      console.log('UI', st);
+      super()
+      this.name = 'ui'
+      this.x = 0
+      this.y = 0
+      this.w = $.cosmos.rx(1)
+      this.h = $.cosmos.ry(1)
+  }
+
+  init() {
+      log('setting up ui')
+      this.spawn('hud/gadget/ImageButton', {
+          name: 'newGame',
+          img: res.ui.start,
+          scale: 2,
+          x: $.cosmos.rx(.5),
+          y: $.cosmos.ry(.5),
+
+          onClick: function() {
+              log('click')
+          }
+      })
   }
 
   evo(dt) {
+  }
 
+  drawBackground() {
+      image(res.backgrounds.start_background,
+              0, 0, this.w, this.h)
+      //fill('404040')
+      //rect(0, 0, this.w, this.h)
+      //background(res.backgrounds.start_background)
   }
 
   draw() {
-      background(res.backgrounds.start_background)
+      super.draw()
   }
 }
